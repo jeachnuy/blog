30428bd5451b2c933c6dc76ce1e27f7dadd013d4
jeachnuy <jae_0107@naver.com>
2022-07-03 12:01:58 +0900

    main page

--- src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -4,15 +4,14 @@ import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
 import com.cos.blog.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.function.Supplier;
 
@@ -20,6 +19,52 @@ import java.util.function.Supplier;
 public class DummyControllerTest {
     @Autowired //의존성 주(DI)
     private UserRepository userRepository;
+
+    @DeleteMapping("/dummy/user/{id}")
+    public String delete(@PathVariable int id) {
+        try {
+        userRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            return "삭제에 실패하였습니다. 해당 id는 DB에 존재하지 않습니다.";
+        }
+        return "삭제 되었습니다. id: " + id;
+    }
+//    @PutMapping("/dummy/user/{id}")
+//    public User updateUser(@PathVariable int id, @RequestBody User requestUser) {
+//        System.out.println("id: " + id);
+//        System.out.println("password: " + requestUser.getPassword());
+//        System.out.println("email: " + requestUser.getEmail());
+//
+//        User user = userRepository.findById(id)
+//                                .orElseThrow(() -> new IllegalArgumentException("수정에 실패하였습니다."));
+//        user.setPassword(requestUser.getPassword());
+//        user.setEmail(requestUser.getEmail());
+//        //save함수는 id를 전달하지 않으면 isert를 해주고
+//        //save함수는 id를 전달하면 해당 id에 대한 데이터가 있으면 udate를 해주고
+//        //save함수는 id를 전달하면 해당 id에 대한 데이터가 없으면 isert를 해준다
+//        userRepository.save(requestUser);
+//        return null;
+//    }
+
+    @Transactional //함수 좋료시에 자동 commit
+    @PutMapping("/dummy/user/{id}")
+    public User updateUser(@PathVariable int id, @RequestBody User requestUser) {
+        System.out.println("id: " + id);
+        System.out.println("password: " + requestUser.getPassword());
+        System.out.println("email: " + requestUser.getEmail());
+
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("수정에 실패하였습니다."));
+        user.setPassword(requestUser.getPassword());
+        user.setEmail(requestUser.getEmail());
+        //save함수는 id를 전달하지 않으면 isert를 해주고
+        //save함수는 id를 전달하면 해당 id에 대한 데이터가 있으면 udate를 해주고
+        //save함수는 id를 전달하면 해당 id에 대한 데이터가 없으면 isert를 해준다
+//        userRepository.save(requestUser);
+
+        //더티 체킹
+        return user;
+    }
 
     @GetMapping("/dummy/user")
     public List<User> list() {
@@ -49,7 +94,7 @@ public class DummyControllerTest {
 
         //람다
         User user = userRepository.findById(id)
-                                .orElseThrow(() -> new IllegalArgumentException("해당 유저는 없습니다. id: " + id));
+                                .orElseThrow(() -> new IllegalArgumentException("해당 사용자는 없습니다. id: " + id));
         //요청:웹브라우저
         //user 객체=자바 오브젝트
         //변환 (웹브라우저가 이해할 수 있는 데이) -> json(Gson라이브러리)
